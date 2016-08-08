84eb812b96b5cd85132a830bdff14c323a02fafa
tyru <tyru.exe@gmail.com>
2016-08-08 22:52:36 +0900

    <textarea readonly>

--- static/page/top.js
+++ static/page/top.js
@@ -21,10 +21,8 @@
           } else {
             try {
               inputFn.json = angular.fromJson(value)
-              inputFn.error = null
             } catch (e) {
               console.error(e)
-              inputFn.error = e
             }
           }
         }
